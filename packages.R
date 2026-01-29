9a24d42ee612040f0825fa160a5f14cc21db48d5
Arnaud FELDMANN <arnaud.feldmann@gmail.com>
2026-01-29 11:14:05 +0100

    essai arrow

--- packages.R
+++ packages.R
@@ -1,6 +1,15 @@
-paste(
-  sort(
-    c(tools::package_dependencies(packages = "ggplot2", recursive = TRUE)$ggplot2, "ggplot2",
-      tools::package_dependencies(packages = "dplyr",recursive = TRUE)$dplyr, "dplyr")
-    ),
-  collapse = " ")
+package_and_dependencies <-
+  function(package) {
+    c(tools::package_dependencies(packages = "ggplot2", recursive = TRUE)$ggplot2, "ggplot2")
+  }
+
+packages_and_dependencies <-
+  function(...) {
+    list(...) |>
+      lapply(package_and_dependencies) |>
+      do.call(c, args=_) |>
+      sort() |>
+      paste(collapse = " ")
+  }
+
+packages_and_dependencies("ggplot2", "dplyr", "arrow")
